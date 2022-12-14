faa26fad9feebc661659a8412b40d183ae437490
ztshort <ztshort@hotmail.com>
2022-12-13 21:59:02 -0600

    Remove console.log

--- src/utils/getLibrary.ts
+++ src/utils/getLibrary.ts
@@ -3,6 +3,5 @@ import { Web3Provider } from '@into-the-fathom/providers'
 export default function getLibrary(provider: any): Web3Provider {
   const library = new Web3Provider(provider, 'any')
   library.pollingInterval = 50
-  console.log(library)
   return library
 }
