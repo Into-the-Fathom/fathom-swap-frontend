d304f6aa702369dde759b1a1a29b497ce374a1bc
ztshort <ztshort@hotmail.com>
2022-12-13 21:14:03 -0600

    Decrease polling interval

--- src/utils/getLibrary.ts
+++ src/utils/getLibrary.ts
@@ -2,6 +2,7 @@ import { Web3Provider } from '@into-the-fathom/providers'
 
 export default function getLibrary(provider: any): Web3Provider {
   const library = new Web3Provider(provider, 'any')
-  library.pollingInterval = 100
+  library.pollingInterval = 50
+  console.log(library)
   return library
 }
